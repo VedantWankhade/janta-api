ceb2c8126d3c16e87a8becc58f6df8df637c20b2
VedantWankhade <vedantwankhade17@gmail.com>
2021-06-03 00:34:12 +0530

    Simplest GraphQL api

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 
 const PORT = process.env.PORT || 4000;
 const app = express();
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello, World!'
+    }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
 app.get('/', (req, res) => res.send('Hello World!!!!!'));
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+server.applyMiddleware({ app, path:'/api' });
+
+app.listen(PORT, () => console.log(`GraphQL server running at http://localhost:${PORT}${server.graphqlPath}`));
