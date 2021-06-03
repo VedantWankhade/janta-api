dccc1ebcf9cd53b7a5715359434567f4c90c807a
VedantWankhade <vedantwankhade17@gmail.com>
2021-06-03 22:16:15 +0530

    Seperate GraphQL schema

---
+++ gqlSchema.js
@@ -0,0 +1,17 @@
+const { gql } = require('apollo-server-express');
+
+module.exports = gql`
+    type Note {
+        id: ID!
+        content: String!
+        author: String!
+    }
+    type Query {
+        about: String
+        notes: [Note!]!
+        note(id: ID!): Note!
+    }
+    type Mutation {
+        newNote(content: String!): Note!
+    }
+`;
