ce90cc78641722ec390809f13427adc9abd6a879
VedantWankhade <vedantwankhade17@gmail.com>
2021-06-03 22:18:22 +0530

    Modularized imports

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
+// Schema
+const typeDefs = require('./gqlSchema');
+// Resolvers
+const resolvers = require('./resolvers');
 // For connecting to the database
 const db = require('./db');
 // Database models
@@ -11,47 +15,19 @@ const API_SERVER_PORT = process.env.API_SERVER_PORT || 4000;
 // Database url
 const DB_URL = process.env.DB_URL;
 
-// GraphQL schema
-const typeDefs = gql`
-    type Note {
-        id: ID!
-        content: String!
-        author: String!
-    }
-    
-    type Query {
-        about: String
-        notes: [Note!]!
-        note(id: ID!): Note!
-    }
-    
-    type Mutation {
-        newNote(content: String!): Note!
-    }
-`;
-
-// GraphQL query resolver object
-const resolvers = {
-    Query: {
-        about: () => 'JANTA GraphQL api v1.0.0',
-        notes: async () => await models.Note.find(),
-        note: async (parent, args) => models.Note.findById(args.id)
-    },
-    Mutation: {
-        newNote: async (parent, args) => await models.Note.create({
-            content: args.content,
-            author: 'Me'
-        })
-    }
-}
-
 db.connect(DB_URL);
-
 const app = express();
 // Apollo server for api
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    // Pass models to resolvers
+    context: () => {
+        return { models }
+    }
+});
 
-app.get('/', (req, res) => res.send('Hello World!!!!!'));
-server.applyMiddleware({ app, path:'/api' });
+app.get('/', (req, res) => res.send('GraphQL api server for JANTA'));
+server.applyMiddleware({ app, path:'/api'});
 
-app.listen(API_SERVER_PORT, () => console.log(`GraphQL server running at http://localhost:${API_SERVER_PORT}${server.graphqlPath}`));
+app.listen(API_SERVER_PORT , () => console.log(`GraphQL server running at http://localhost:${API_SERVER_PORT}${server.graphqlPath}`));
