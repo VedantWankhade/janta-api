41abf4100abc68d29d2c618721d203a26eb45298
VedantWankhade <vedantwankhade17@gmail.com>
2021-06-04 00:59:34 +0530

    Added signIn mutation resolver

--- resolvers/mutation.js
+++ resolvers/mutation.js
@@ -61,5 +61,26 @@ module.exports = {
             console.error(err);
             throw new Error('Error creating account');
         }
+    },
+    signIn: async (_, { username, email, password }, models) => {
+        if (email) {
+            // Convert email into format similiar to the one in DB
+            email = email.trim().toLowerCase();
+        }
+        // Find user in DB by email or username or both
+        const user = await models.User.findOne({
+            $or: [{ email }, { username }]
+        });
+        // If user is not found, throw an authentication error
+        if (!user) {
+            throw new AuthenticationError('User account not found');
+        }
+        // If passwords don't match, throw an authentication error
+        const valid = await bcrypt.compare(password, user.password);
+        if (!valid) {
+            throw new AuthenticationError('Wrong password');
+        }
+        // Else create and return the jwt
+        return jwt.sign({ id: user._id }, JWT_SECRET);
     }
 }
